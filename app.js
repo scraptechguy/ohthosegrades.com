23aa529f341b072463d83e3ed815d1c8d8231924
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-12-09 11:25:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 let result_div = document.getElementById("result");
-let result = 0
 
-const pipeline_div = document.getElementById("pipeline");
+var weight1 = document.getElementById("weight-1");
+var weightOutput1 = document.getElementById("output1");
+weightOutput1.innerHTML = weight1.value; 
+
+var grade1 = document.getElementById("grade-1")
+var gradeOutput1 = document.getElementById("output2");
+gradeOutput1.innerHTML = grade1.value;
+
 
 function main() {
+    weight1.oninput = function() {
+        weightOutput1.innerHTML = this.value;
+    }
+
+    grade1.oninput = function() {
+        gradeOutput1.innerHTML = this.value;
+    }
+
+    result_div.oninput = function() {
+        result_div.innerHTML = (weight1.value/100) * (grade1.value/100)
+    }
 }
 
 main();
